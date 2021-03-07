7b8f8de676cac6778380f28396e44a2e8fb2491b
no0g <tp048892@mail.apu.edu.my>
2021-03-07 19:12:56 +0800

    fix: delete all payment record before deleting contract

--- controller/admin/contract/delete.php
+++ controller/admin/contract/delete.php
@@ -8,19 +8,50 @@
         include "../../../config/config.php";
         $id = $_GET['id'];
         $srcpath = "location: ../../../admin/contract/edit/?id=".$id;
-        $delete = $mysqli->prepare("DELETE FROM contract WHERE id = ?");
+
+        //delete related payment submission
+        $delete = $mysqli->prepare("DELETE FROM payment WHERE contract = ?");
         $delete->bind_param('i', $id);
         $result = $delete->execute();
         if($result) {
-            
+            $delete = $mysqli->prepare("DELETE FROM contract WHERE id = ?");
+            $delete->bind_param('i', $id);
+            $result = $delete->execute();
+            if($result) {
+                
+                // Success
+                header($srcpath."&message=successdelete");
+                $delete->close();
+            } else {
+                // If Error Occured
+                header($srcpath."&message=faildelete");
+                $delete->close();
+            }
             // Success
             header($srcpath."&message=successdelete");
             $delete->close();
         } else {
+
+            $delete = $mysqli->prepare("DELETE FROM contract WHERE id = ?");
+            $delete->bind_param('i', $id);
+            $result = $delete->execute();
+            if($result) {
+                
+                // Success
+                header($srcpath."&message=successdelete");
+                $delete->close();
+            } else {
+                // If Error Occured
+                header($srcpath."&message=faildelete");
+                $delete->close();
+            }
             // If Error Occured
             header($srcpath."&message=faildelete");
             $delete->close();
         }
+
+
+        
         
 
     }
