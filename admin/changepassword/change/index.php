3a1fb3974174455b2e14ddb4abe4a178d5cdce02
no0g <tp048892@mail.apu.edu.my>
2021-03-08 18:12:39 +0800

    fix: page title

--- admin/changepassword/change/index.php
+++ admin/changepassword/change/index.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <title>Inputs - Sleek Admin Dashboard Template</title>
+  <title>Change Password</title>
   <script src="../../../assets/js/passwordchecker.js"></script>
   <!-- GOOGLE FONTS -->
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500|Poppins:400,500,600,700|Roboto:400,500" rel="stylesheet"/>
--- admin/roomtype/new/index.php
+++ admin/roomtype/new/index.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <title>Your Contract</title>
+  <title>New Roomtype</title>
 
   <!-- GOOGLE FONTS -->
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500|Poppins:400,500,600,700|Roboto:400,500" rel="stylesheet"/>
